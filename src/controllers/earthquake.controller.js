859c83d9f42aad8969376ad4e40b07bb28eac16c
Eray <95094747+eraysolenkol@users.noreply.github.com>
2023-02-09 20:11:37 +0300

    Update earthquake.controller.js

--- src/controllers/earthquake.controller.js
+++ src/controllers/earthquake.controller.js
@@ -2,7 +2,7 @@ import EarthquakeWrapper from "../services/earthquake_wrapper.js";
 
 
 async function getLastEarthquakes(req, res, next) {
-    let result;
+    let response;
     if (req.query.limit == null) {
         console.error(`Error: limit required in query`);
         return res.json({Error:"limit query required"});
@@ -10,11 +10,11 @@ async function getLastEarthquakes(req, res, next) {
 
     try {
         if (req.query.min_magnitude == null) {
-            result = await EarthquakeWrapper.getLastEarthquakes(req.query.limit);
+            response = await EarthquakeWrapper.getLastEarthquakes(req.query.limit);
         } else {
-            result = await EarthquakeWrapper.getLastEarthquakesOverMagnitude(req.query.limit,req.query.min_magnitude);
+            response = await EarthquakeWrapper.getLastEarthquakesOverMagnitude(req.query.limit,req.query.min_magnitude);
         }
-        res.json(result);
+        res.json(response);
     } catch (err) {
         res.json({"Error":err.message});
         next(err);
